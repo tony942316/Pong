bd0f09e8d8a22fe6791676866685d5785d609e98
tony942316 <grassoa159@gmail.com>
2023-09-01 23:32:32 +0000

    Add Dependencies

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
+#include "Pulsar/Pulsar.hpp"
+
+constinit inline auto g_Window = pul::Window();
+
+void update() noexcept
+{
+
+}
+
+void render() noexcept
+{
+
+}
 
 int main()
 {
     std::cout << "Start" << std::endl;
 
+    g_Window.init(1200, 800, "Pong");
+    g_Window.setUpdateFunc(update);
+    g_Window.setRenderFunc(render);
 
+    g_Window.show();
 
     std::cout << "End" << std::endl;
     return 0;
